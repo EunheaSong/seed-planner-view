99d552880b721fd6be0ccffc2155d727653378c8
song-eunhye <dmsgp8292@gmail.com>
2024-03-16 23:16:59 +0900

    [UPDATE] - 회원가입 정리

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -2,6 +2,7 @@ import {SignUpBox} from "../style/member/SignStyle";
 import {useState} from "react";
 import {signUp} from "../api/member/auth";
 
+
 export default function SignUp() {
 
     const [formData, setFormData] = useState({
@@ -12,12 +13,13 @@ export default function SignUp() {
 
     // 입력 필드 값 변경 핸들러
     const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
+        const {name, value} = event.target;
+        setFormData({...formData, [name]: value});
     };
 
     // 폼 제출 핸들러
     const handleSubmit = (event) => {
+        event.preventDefault();
         console.log(formData.email);
         signUp(formData);
     }
@@ -25,32 +27,32 @@ export default function SignUp() {
     return (
         <SignUpBox>
             <h1>회원가입</h1>
-                <div>
-                    <label>email</label>
-                    <input type="email"
-                           placeholder="Email"
-                           name="email"
-                           value={formData.email}
-                           onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>비밀번호</label>
-                    <input type="password" placeholder="비밀번호" maxLength={12}
-                           name="password"
-                           value={formData.password}
-                           onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label>비밀번호 재확인</label>
-                    <input type="password" placeholder="비밀번호 재확인" maxLength={12}
-                           name="rePassword"
-                           value={formData.rePassword}
-                           onChange={handleChange}
-                    />
-                </div>
-                <button type="submit" onClick={handleSubmit}>가입하기</button>
+            <div>
+                <label>email</label>
+                <input type="email"
+                       placeholder="Email"
+                       name="email"
+                       value={formData.email}
+                       onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>비밀번호</label>
+                <input type="password" placeholder="비밀번호" maxLength={12}
+                       name="password"
+                       value={formData.password}
+                       onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>비밀번호 재확인</label>
+                <input type="password" placeholder="비밀번호 재확인" maxLength={12}
+                       name="rePassword"
+                       value={formData.rePassword}
+                       onChange={handleChange}
+                />
+            </div>
+            <button type="submit" onClick={handleSubmit}>가입하기</button>
         </SignUpBox>
     )
 }
